2e263c839518aa74c29d5328577530b42d2c3433
davidk7680 <davidk7680@hstat.org>
2017-10-11 02:25:52 +0000

    finish lab

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,23 @@
-def division
-    
+def division(first_num, second_num)
+    first_num/second_num
 end
 
-def argue
-  
+def argue(arguement)
+    arguement
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-    
+    return "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(slice="cheese")
+    slice
 end
 
-def whisper
-    
+def whisper(arguement)
+    arguement.downcase
 end
